93a97f3c37e695bd44ff4f68d800082e0902b841
luhuipeng <973880264@qq.com>
2022-09-24 16:54:44 +0800

    feat(compiler-core): parse element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType {
+  Start,
+  End,
+}
+
 export function baseParse(content: string) {
   const context = createParseContext(content);
   return createRoot(parseChildren(context));
@@ -7,14 +12,38 @@ export function baseParse(content: string) {
 function parseChildren(context) {
   const nodes: any = [];
   let node;
-
-  if (context.source.startsWith("{{")) {
+  let s = context.source;
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context);
+  } else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
   }
   nodes.push(node);
 
   return nodes;
 }
+
+function parseElement(context: any) {
+  // 1. 解析 tag   2.删除处理完成的string
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End);
+  return element;
+}
+
+function parseTag(context: any, tagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+  if (tagType === TagType.End) return;
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
+}
+
 function parseInterpolation(context) {
   // {{message}}
   const openDelimiter = "{{";
