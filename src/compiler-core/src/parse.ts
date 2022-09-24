0d5b7a585b03e39831db876b7b834dfe8f3d6821
luhuipeng <973880264@qq.com>
2022-09-24 15:58:55 +0800

    feat(compiler-core): parse interpolation

---
+++ src/compiler-core/src/parse.ts
@@ -0,0 +1,54 @@
+import { NodeTypes } from "./ast";
+
+export function baseParse(content: string) {
+  const context = createParseContext(content);
+  return createRoot(parseChildren(context));
+}
+function parseChildren(context) {
+  const nodes: any = [];
+  let node;
+
+  if (context.source.startsWith("{{")) {
+    node = parseInterpolation(context);
+  }
+  nodes.push(node);
+
+  return nodes;
+}
+function parseInterpolation(context) {
+  // {{message}}
+  const openDelimiter = "{{";
+  const closeDelimiter = "}}";
+  const closeIndex = context.source.indexOf(
+    closeDelimiter,
+    openDelimiter.length
+  );
+
+  // slice(start, end)
+  advanceBy(context, openDelimiter.length);
+  const rawContentLength = closeIndex - openDelimiter.length;
+  const rawContent = context.source.slice(0, rawContentLength);
+  const content = rawContent.trim();
+  advanceBy(context, rawContentLength + closeDelimiter.length);
+
+  return {
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content,
+    },
+  };
+}
+function advanceBy(context, length) {
+  context.source = context.source.slice(length);
+}
+function createRoot(children) {
+  return {
+    children,
+  };
+}
+function createParseContext(content: string): any {
+  return {
+    source: content,
+  };
+}
